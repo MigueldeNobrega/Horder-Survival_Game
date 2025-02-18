a878bf1acb4a81ecdff02aa5df0b75c8dbb0c21c
Liquix7 <liquix97099@gmail.com>
2025-02-18 20:14:21 +0100

    MenuCreditos y barraEfectos sonido

--- Assets/Scripts/MenuOpciones.cs
+++ Assets/Scripts/MenuOpciones.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class MenuOpciones : MonoBehaviour
 {
+
+
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioMixer audioMixerEfectos;
+
+    public void ChangeSceneCreditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+    public void ChangeSceneMenu()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+ 
     public void CambiarVolumen(float volumen)
     {
         audioMixer.SetFloat("volumen",volumen);
+    }
+
+    public void CambiarVolumenEfectos(float VolumenEfectos)
+    {
+        audioMixerEfectos.SetFloat("VolumenEfectos", VolumenEfectos);
     }
 }
